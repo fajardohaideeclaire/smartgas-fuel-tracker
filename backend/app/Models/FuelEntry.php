5080104fe2c181f54425af30a8f78ef84c18d933
Haidee Claire Fajardo <fajardohaideeclaire@gmail.com>
2026-04-01 10:57:02 +0800

    feat: create FuelEntry model

---
+++ backend/app/Models/FuelEntry.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelEntry extends Model
+{
+   use HasFactory;
+
+   protected $fillable = [
+       'user_id',
+       'station_name',
+       'fuel_type',
+       'price_per_liter'
+   ];
+}
